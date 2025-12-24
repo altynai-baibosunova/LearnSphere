1638833cdef5710203d541c70b7df9633c6dd2ca
Altynai <harmoni5413@gmail.com>
2025-12-24 13:47:09 -0600

    Add RegisterRequest

---
+++ src/main/java/com/example/demo/dto/RegisterDto.java
@@ -0,0 +1,13 @@
+package com.example.demo.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterDto {
+    private String username;
+    private String password;
+}
+
